3ab79b060d538a378511ae6eb288562d557e36de
Daphne Gripon <daphne.gripon@devinci.fr>
2015-09-17 17:44:18 +0200

    Update Exercices 1 & 2

--- WS1/EX1/script.js
+++ WS1/EX1/script.js
@@ -1,105 +1,113 @@
 'use strict';
 
-function load()
+//Variables
+var data = {
+  "cars": [
+    {
+      "id": "p306",
+      "vehicule": "peugeot 306",
+      "pricePerDay": 20,
+      "pricePerKm": 0.10
+    },
+    {
+      "id": "rr-sport",
+      "pricePerDay": 60,
+      "pricePerKm": 0.30
+    },
+    {
+      "id": "p-boxster",
+      "pricePerDay": 100,
+      "pricePerKm": 0.45
+    }
+  ],
+  "rentals": [
+    {
+      "id": "1-pb-92",
+      "driver": {
+        "firstName": "Paul",
+        "lastName": "Bismuth"
+      },
+      "car_id": "p306",
+      "pickupDate": "2015-09-12",
+      "returnDate": "2015-09-14",
+      "distance": 150
+    },
+    {
+      "id": "2-rs-92",
+      "driver": {
+        "firstName": "Rebecca",
+        "lastName": "Solanas"
+      },
+      "car_id": "rr-sport",
+      "pickupDate": "2015-09-09",
+      "returnDate": "2015-09-13",
+      "distance": 550
+    },
+    {
+      "id": "3-sa-92",
+      "driver": {
+        "firstName": " Sami",
+        "lastName": "Ameziane"
+      },
+      "car_id": "p-boxster",
+      "pickupDate": "2015-09-12",
+      "returnDate": "2015-09-14",
+      "distance": 100
+    }
+  ]
+};
+var cars = data.cars;
+var rentals = data.rentals;
+
+console.log(data);
+
+var result = '';
+for(var i = 0; i < rentals.length; i++)
 {
-  var paragraph = document.getElementById("rentalsDetails");
-  paragraph.innerHTML =  "Rentals:<br><br>";
-  var data = {
-    "cars": [
-      {
-        "id": "p306",
-        "vehicule": "peugeot 306",
-        "pricePerDay": 20,
-        "pricePerKm": 0.10
-      },
-      {
-        "id": "rr-sport",
-        "pricePerDay": 60,
-        "pricePerKm": 0.30
-      },
-      {
-        "id": "p-boxster",
-        "pricePerDay": 100,
-        "pricePerKm": 0.45
-      }
-    ],
-    "rentals": [
-      {
-        "id": "1-pb-92",
-        "driver": {
-          "firstName": "Paul",
-          "lastName": "Bismuth"
-        },
-        "car_id": "p306",
-        "pickupDate": "2015-09-12",
-        "returnDate": "2015-09-14",
-        "distance": 150
-      },
-      {
-        "id": "2-rs-92",
-        "driver": {
-          "firstName": "Rebecca",
-          "lastName": "Solanas"
-        },
-        "car_id": "rr-sport",
-        "pickupDate": "2015-09-09",
-        "returnDate": "2015-09-13",
-        "distance": 550
-      },
-      {
-        "id": "3-sa-92",
-        "driver": {
-          "firstName": " Sami",
-          "lastName": "Ameziane"
-        },
-        "car_id": "p-boxster",
-        "pickupDate": "2015-09-12",
-        "returnDate": "2015-09-14",
-        "distance": 100
-      }
-    ]
-  };
-  var cars = data.cars;
-  var rentals = data.rentals;
+  result += printRentalInfo(rentals[i]);
+}
 
-  console.log(data);
+document.write(result);
 
-  var id = 'N/A';
-  var driver = 'N/A';
-  var pickupDate = 'N/A';
-  var returnDate = 'N/A';
-  var car_id = 'N/A';
-  var distance = 'N/A';
+/*
+* @function returns the information and price for a particular rental.
+* @param : rental : the rental object, from the json variable, containing
+*                   useful information
+*/
+function printRentalInfo(rental)
+{
+  var result = '';
   var pricePerKm = 0;
   var pricePerDay = 0;
   var price = 0;
-  var duration = 0;
+  var driver = rental.driver.firstName + ' ' + rental.driver.lastName;
+  var pickupDate = new Date(rental.pickupDate);
+  var returnDate = new Date(rental.returnDate);
+  var duration =  getDayDifference( pickupDate, returnDate);
 
-  for(var i = 0; i < rentals.length; i++)
+  for(var j = 0; j < cars.length; j++)
   {
-    id = rentals[i].id;
-    car_id = rentals[i].car_id;
-    distance = rentals[i].distance;
-    driver = rentals[i].driver.firstName + ' ' + rentals[i].driver.lastName;
-    pickupDate = rentals[i].pickupDate;
-    returnDate = rentals[i].returnDate;
-    var d1 = new Date(pickupDate);
-    var d2 = new Date(returnDate);
-    duration = (d2.getTime() - d1.getTime()) / (60 * 60 * 24 * 1000) + 1;
-
-    for(var j = 0; j < cars.length; j++)
-    {
-        if(cars[j].id == car_id)
-        {
-          pricePerKm = cars[j].pricePerKm;
-          pricePerDay = cars[j].pricePerDay;
-          price = distance * pricePerKm + duration * pricePerDay;
-          break;
-        }
-    }
-    paragraph.innerHTML +=  driver + " rented the " + car_id + " car for ";
-    paragraph.innerHTML +=  duration + "days(" +pricePerDay+ "/day) and drove ";
-    paragraph.innerHTML +=  distance + "km("+pricePerKm+ "/km) for a total of ";
-    paragraph.innerHTML +=  "<span style='color:red;font-weight:bold;'>" + price + "</span><br>";
+      if(cars[j].id == rental.car_id)
+      {
+        pricePerKm = cars[j].pricePerKm;
+        pricePerDay = cars[j].pricePerDay;
+        price = rental.distance * pricePerKm + duration * pricePerDay;
+        result +=  driver + " rented the " + rental.car_id + " car for ";
+        result +=  duration + "days(" + pricePerDay + "/day) and drove ";
+        result +=  rental.distance + "km(" + pricePerKm + "/km) for a total of ";
+        result +=  "<span style='color:red;font-weight:bold;'>" +price+ "</span><br>";
+        break;
+      }
   }
-}
+  return result;
+};
+
+/*
+* @function returns how many days the date1 to date2 period makes
+* @param : date1 : the start date of the period
+* @param : date2 : the end date of the period
+*/
+function getDayDifference(date1, date2)
+{
+  return (date2.getTime() - date1.getTime()) / (60 * 60 * 24 * 1000) + 1;
+};
--- WS1/EX2/script.js
+++ WS1/EX2/script.js
@@ -1,118 +1,122 @@
 'use strict';
 
-function load()
+var data = {
+  "cars": [
+    {
+      "id": "p306",
+      "vehicule": "peugeot 306",
+      "pricePerDay": 20,
+      "pricePerKm": 0.10
+    }
+  ],
+  "rentals": [
+    {
+      "id": "1-pb-92",
+      "driver": {
+        "firstName": "Paul",
+        "lastName": "Bismuth"
+      },
+      "car_id": "p306",
+      "pickupDate": "2015-09-12",
+      "returnDate": "2015-09-12",
+      "distance": 100
+    },
+    {
+      "id": "2-rs-92",
+      "driver": {
+        "firstName": "Rebecca",
+        "lastName": "Solanas"
+      },
+      "car_id": "p306",
+      "pickupDate": "2015-09-10",
+      "returnDate": "2015-09-15",
+      "distance": 300
+    },
+    {
+      "id": "3-sa-92",
+      "driver": {
+        "firstName": " Sami",
+        "lastName": "Ameziane"
+      },
+      "car_id": "p306",
+      "pickupDate": "2015-08-31",
+      "returnDate": "2015-09-13",
+      "distance": 1000
+    }
+  ]
+};
+console.log(data);
+var cars = data.cars;
+var rentals = data.rentals;
+
+var result = '';
+for(var i = 0; i < rentals.length; i++)
 {
-  var paragraph = document.getElementById("rentalsDetails");
-  var data = {
-    "cars": [
-      {
-        "id": "p306",
-        "vehicule": "peugeot 306",
-        "pricePerDay": 20,
-        "pricePerKm": 0.10
-      }
-    ],
-    "rentals": [
-      {
-        "id": "1-pb-92",
-        "driver": {
-          "firstName": "Paul",
-          "lastName": "Bismuth"
-        },
-        "car_id": "p306",
-        "pickupDate": "2015-09-12",
-        "returnDate": "2015-09-12",
-        "distance": 100
-      },
-      {
-        "id": "2-rs-92",
-        "driver": {
-          "firstName": "Rebecca",
-          "lastName": "Solanas"
-        },
-        "car_id": "p306",
-        "pickupDate": "2015-09-10",
-        "returnDate": "2015-09-15",
-        "distance": 300
-      },
-      {
-        "id": "3-sa-92",
-        "driver": {
-          "firstName": " Sami",
-          "lastName": "Ameziane"
-        },
-        "car_id": "p306",
-        "pickupDate": "2015-08-31",
-        "returnDate": "2015-09-13",
-        "distance": 1000
-      }
-    ]
-  };
-  console.log(data);
-  var cars = data.cars;
-  var rentals = data.rentals;
+  result += printRentalInfo(rentals[i]);
+}
 
+document.write(result);
 
-  var id = 'N/A';
-  var driver = 'N/A';
-  var pickupDate = 'N/A';
-  var returnDate = 'N/A';
-  var car_id = 'N/A';
-  var distance = 'N/A';
+/*
+* @function returns the information and price for a particular rental.
+* @param : rental : the rental object, from the json variable, containing
+*                   useful information
+*/
+function printRentalInfo(rental)
+{
+  var result = '';
   var pricePerKm = 0;
   var pricePerDay = 0;
   var price = 0;
-  var duration = 0;
-  paragraph.innerHTML =  "Rentals:<br><br>";
-  for(var i = 0; i < rentals.length; i++)
+  var driver = rental.driver.firstName + ' ' + rental.driver.lastName;
+  var pickupDate = new Date(rental.pickupDate);
+  var returnDate = new Date(rental.returnDate);
+  var duration =  getDayDifference( pickupDate, returnDate);
+
+  for(var j = 0; j < cars.length; j++)
   {
-    console.log(rentals[i]);
-    id = rentals[i].id;
-    car_id = rentals[i].car_id;
-    distance = rentals[i].distance;
-    driver = rentals[i].driver.firstName + ' ' + rentals[i].driver.lastName;
-    pickupDate = rentals[i].pickupDate;
-    returnDate = rentals[i].returnDate;
-    var d1 = new Date(pickupDate);
-    var d2 = new Date(returnDate);
-    duration = (d2.getTime() - d1.getTime()) / 86400000 + 1;
+      if(cars[j].id == rental.car_id)
+      {
+        pricePerKm = cars[j].pricePerKm;
+        pricePerDay = cars[j].pricePerDay;
+        var durationPrice = 0;
+        var tenPercent = pricePerDay - 10*pricePerDay/100;
+        var thirtyPercent = pricePerDay - 30*pricePerDay/100;
+        var fiftyPercent = pricePerDay - 50*pricePerDay/100;
 
-    for(var j = 0; j < cars.length; j++)
-    {
-        if(cars[j].id == car_id)
+        if(duration <= 1)
         {
-          pricePerKm = cars[j].pricePerKm;
-          pricePerDay = cars[j].pricePerDay;
-
-          var durationPrice = 0;
-          var tenPercent = pricePerDay - 10*pricePerDay/100;
-          var thirtyPercent = pricePerDay - 30*pricePerDay/100;
-          var fiftyPercent = pricePerDay - 50*pricePerDay/100;
-
-          if(duration <= 1)
-          {
-            durationPrice = pricePerDay;
-          }
-          else if(duration <= 4)
-          {
-            durationPrice = pricePerDay + (duration - 1) * (tenPercent);
-          }
-          else if(duration <= 10)
-          {
-            durationPrice = pricePerDay + 3 * (tenPercent) + (duration - 4)*thirtyPercent;
-          }
-          else
-          {
-            durationPrice = pricePerDay + 3 * (tenPercent) + 6 * thirtyPercent + (duration - 10) * fiftyPercent;
-          }
-          price = distance * pricePerKm + durationPrice;
-          alert(distance+"*"+ pricePerKm+"+"+durationPrice+"="+price);
-          break;
+          durationPrice = pricePerDay;
         }
-    }
-    paragraph.innerHTML +=  driver + " rented the " + car_id + " car for ";
-    paragraph.innerHTML +=  duration + "days and drove ";
-    paragraph.innerHTML +=  distance + "km for a total of ";
-    paragraph.innerHTML +=  "<span style='color:red;font-weight:bold;'>" + price + "</span><br>";
+        else if(duration <= 4)
+        {
+          durationPrice = pricePerDay + (duration - 1) * (tenPercent);
+        }
+        else if(duration <= 10)
+        {
+          durationPrice = pricePerDay + 3 * (tenPercent) + (duration - 4)*thirtyPercent;
+        }
+        else
+        {
+          durationPrice = pricePerDay + 3 * (tenPercent) + 6 * thirtyPercent + (duration - 10) * fiftyPercent;
+        }
+        price = rental.distance * pricePerKm + durationPrice;
+        result +=  driver + " rented the " + rental.car_id + " car for ";
+        result +=  duration + "days(" + pricePerDay + "/day) and drove ";
+        result +=  rental.distance + "km(" + pricePerKm + "/km) for a total of ";
+        result +=  "<span style='color:red;font-weight:bold;'>" +price+ "</span><br>";
+        break;
+      }
   }
-}
+  return result;
+};
+
+/*
+* @function returns how many days the date1 to date2 period makes
+* @param : date1 : the start date of the period
+* @param : date2 : the end date of the period
+*/
+function getDayDifference(date1, date2)
+{
+  return (date2.getTime() - date1.getTime()) / (60 * 60 * 24 * 1000) + 1;
+};
